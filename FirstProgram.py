be4d61deca711da596360882c5a9019b8b07da00
ParamShah101 <tryaitools101@gmail.com>
2025-08-18 20:31:37 +0530

    Add files via upload

---
+++ FirstProgram.py
@@ -0,0 +1,23 @@
+# Python is a popular programming language. It was created by Guido Van Rossum in 1991
+
+# Current Version of Python is 3.13.7
+
+print("Param Shah")
+
+# Want To Check Python Version
+
+import sys
+
+print(sys.version)
+
+# Some Questions
+
+"""
+    What is the correct file extension for Python files? --------> .py
+
+    What is a correct command line syntax for checking if python is installed on your computer?  ------>  python --version
+
+
+    What is a correct syntax to exit the Python command line interface? ------> exit()
+"""
+
